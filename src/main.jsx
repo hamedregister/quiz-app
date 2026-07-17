7527975a9e9b1b1194aa419eba20995b1f992d19
hamedregister <hamed.register@gmail.com>
2026-07-17 19:23:25 +0330

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>وب اپلیکیشن آزمون ساز آنلاین</h1>
+      <p>این پروژه به سوبابیس متصل است و کدهای فرانت شما اینجا قرار می‌گیرند.</p>
+    </div>
+  </React.StrictMode>,
+)
