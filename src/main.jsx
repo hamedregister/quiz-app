c4b97e2089ee1bbcce45a8c5f0d956ee5ea3c7c5
hamedregister <hamed.register@gmail.com>
2026-07-17 19:49:58 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>وب اپلیکیشن آزمون ساز آنلاین</h1>
+      <p>این پروژه با موفقیت روی گیت‌هاب پیجز منتشر شد و به سوبابیس متصل است.</p>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div style={{textAlign: 'center', marginTop: '50px'}}>
-      <h1>وب اپلیکیشن آزمون ساز آنلاین</h1>
-      <p>این پروژه به سوبابیس متصل است و کدهای فرانت شما اینجا قرار می‌گیرند.</p>
-    </div>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
